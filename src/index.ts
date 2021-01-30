477d0368de0ed9b79a5bedc90bd094190e925082
suarasiy <sidiqindrajatiyusuf@gmail.com>
2021-01-30 17:57:01 +0800

    Add CompanyMarker (bad code) + [duplicated]

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { User } from './User';
+import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
 const div_map: Element = document.querySelector("#map");
 
 const user = new User();
+const company = new Company();
 const customMap = new CustomMap(div_map);
 
 customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
