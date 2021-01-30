9980671e3d28e567b2a06a902a6cdb5731ba0a63
suarasiy <sidiqindrajatiyusuf@gmail.com>
2021-01-30 20:54:12 +0800

    Finishing & wrap up

--- src/Company.ts
+++ src/Company.ts
@@ -1,19 +1,35 @@
 import faker from 'faker';
+import { Mappable } from './CustomMap';
 
-export class Company {
-    companyName: string;
-    catchPhrase: string;
-    location: {
-        lat: number;
-        lng: number;
+export class Company implements Mappable {
+  name: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  color: string = 'blue';
+
+  constructor() {
+    this.name = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseInt(faker.address.latitude()),
+      lng: parseInt(faker.address.longitude()),
     };
+  }
 
-    constructor() {
-        this.companyName = faker.company.companyName();
-        this.catchPhrase = faker.company.catchPhrase();
-        this.location = {
-            lat: parseInt(faker.address.latitude()),
-            lng: parseInt(faker.address.longitude())
-        };
-    }
+  markerContent(): string {
+    return `
+      <div>
+        <h1>${this.name}</h1>
+        <p>
+            <span style="font-weight: bold">
+            Catch phrase :
+            </span>
+            ${this.catchPhrase}
+        </p>
+    </div>
+      `;
+  }
 }
